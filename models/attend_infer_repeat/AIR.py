4d2898e5f3f63539af2642fcc34b0aea73c8aebe
Chris Grimm <cgrimm@gmail.com>
2016-07-03 02:48:29 -0400

    added sthings to air

--- models/attend_infer_repeat/AIR.py
+++ models/attend_infer_repeat/AIR.py
@@ -1,44 +1,59 @@
 import tensorflow as tf
 import numpy as np
-from models.spatial_transformer_network.SpatialTransformerNetwork import SpatialTransformerNetwork as STN, fully_connected_localizer
-from models.variational_autoencoder.VariationalAutoencoder import VAE, VAE_realize
+from models.spatial_transformer_network.SpatialTransformerNetwork import SpatialTransformerNetwork as STN
+from models.variational_autoencoder.VariationalAutoencoder import VAE, VAE_realize, log_bernoulli_pmf, log_normal_pdf
 from tf_utils import *
 
 class AIR(object):
 
-
-    def __init__(self, input_height, input_width, lstm_units, max_objects):
+    def __init__(self, x, input_height, input_width, lstm_units, max_objects):
         self.ih, self.iw = input_height, input_width
         self.lstm_u = lstm_units
         self.N = max_objects
 
-    def setup_network(self):
-        self.input = tf.placeholder(tf.float32, [None, self.ih, self.iw])
-        batch_size = tf.shape(self.input)[0]
+    def setup_network(self, x):
+        x = tf.placeholder(tf.float32, [None, self.ih, self.iw])
+        batch_size = tf.shape(x)[0]
         self.lstm = tf.nn.rnn_cell.BasicLSTMCell(self.lstm_u)
         self.init_state = np.zeros([batch_size, self.lstm_u])
         state = self.init_state
+        # the 'initial' pres value cant be trainable.
+        prev_pres = tf.Variable(tf.ones([batch_size, 1]), trainable=False)
+        cum_q_z_pres = tf.Variable(tf.zeros([batch_size, 1]), trainable=False)
+        cum_q_z_where = tf.Variable(tf.zeros([batch_size, 1]), trainable=False)
+        cum_q_z_what = tf.Variable(tf.zeros([batch_size, 1]), trainable=False)
+        cum_p_x_given_z = tf.Variable(tf.zeros([batch_size, 1]), trainable=False)
+        cum_p_z = tf.Variable(tf.zeros([batch_size, 1]), trainable=False)
+        y = tf.zeros_like(x)
         # step through the lstm.
         for iter in range(self.N):
-            output, state = self.lstm(self.input, state)
+            output, state = self.lstm(x, state)
             # set up z_where params
             z_where_params = VAE(output, 100, 3, 'gaussian')
             z_where_random_step = tf.placeholder(tf.float32, [batch_size, 3])
             z_where = VAE_realize(z_where_params, z_where_random_step, 'gaussian')
+            cum_p_z += log_normal_pdf(z_where, tf.ones_like(z_where_params[0]), tf.ones_like(z_where_params[1]))
+            cum_q_z_where += log_normal_pdf(x, z_where_params[0], z_where_params[1])
             # set up z_pres params
-            z_pres_params = VAE(100, 1, 'bernoulli')
+            # multiply by old previous value... (if prev pres is zero for an image, it kills the bernoulli distr)
+            z_pres_params = VAE(output, 100, 1, 'bernoulli')[0] * prev_pres
+            cum_q_z_pres += log_bernoulli_pmf(x, z_pres_params)
             z_pres_random_step = tf.placeholder(tf.float32, [batch_size, 1])
-            z_pres = VAE_realize(z_pres_params, z_pres_random_step, 'bernoulli')
-            pres = connected_layers(output, [100, 1], ['relu', 'sigmoid'])
+            # move the prev_pres variable forward
+            z_pres = prev_pres = VAE_realize([z_pres_params], z_pres_random_step, 'bernoulli')
+            cum_p_z += log_bernoulli_pmf(z_pres, tf.ones_like(z_pres_params))
             # set up z_what params
             where_flat = connected_layers(z_where, [100, 3], ['tanh', 'tanh'])
             where = tf.gather(where_flat, np.mat('0 0 1; 0 0 2')) * np.mat('1 0 1; 0 1 1')
-            stn = STN(self.input, (28, 28))
+            stn = STN(x, (28, 28))
             x_att = stn.transform(where)
-            what_params = VAE(x_att, 500, 20, 'gaussian')
-            what_random_step = tf.placeholder(tf.float32, [batch_size, 20])
-            z_what = VAE_realize(what_params, what_random_step, 'gaussian')
+            z_what_params = VAE(x_att, 500, 20, 'gaussian')
+            z_what_random_step = tf.placeholder(tf.float32, [batch_size, 20])
+            z_what = VAE_realize(z_what_params, z_what_random_step, 'gaussian')
+            cum_p_z += log_normal_pdf(z_what, tf.ones_like(z_what_params[0]), tf.ones_like(z_what_params[1]))
+            cum_q_z_what += log_normal_pdf(x, z_what_params[0], z_what_params[1])
             y_att_flat_params = VAE(z_what, 500, 28*28, 'gaussian')
+            cum_p_x_given_z += log_normal_pdf(x, y_att_flat_params[0], y_att_flat_params[1])
             y_att = tf.reshape(y_att_flat_params[0], [-1, 28, 28])
             inv_where_flat = connected_layers(z_where, [100, 3], ['tanh', 'tanh'])
             inv_where = tf.gather(inv_where_flat, np.mat('0 0 1; 0 0 2')) * np.mat('1 0 1; 0 1 1')
@@ -46,9 +61,7 @@ class AIR(object):
             y_i = inv_stn.transform(inv_where)
             pres_mask = tf.tile(z_pres, [1, 40, 40])
             y_i_masked = y_i * pres_mask
-
-
-
+            y += y_i_masked
 
 
 
--- models/variational_autoencoder/VariationalAutoencoder.py
+++ models/variational_autoencoder/VariationalAutoencoder.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 import numpy as np
 
+def log_normal_pdf(x, mu, diag_sigmas):
+    D = mu.get_shape()[1]
+    exp_part = -0.5 * tf.reduce_sum(tf.mul(tf.mul((x - mu), (1.0/(tf.pow(diag_sigmas,2)))), (x - mu)), reduction_indices=1)
+    return (-D**2 / 2)*tf.log(2*np.pi) - 0.5*tf.reduce_sum(2*tf.log(diag_sigmas), reduction_indices=1) + exp_part
+
+def log_bernoulli_pmf(x, p1):
+    return tf.log(p1 * tf.float32(x == 1) + (1 - p1) * tf.float32(x == 0))
 
 def VAE(input, hiddenSize, codeSize, rvType):
     input_dim = input.get_shape()[1]
