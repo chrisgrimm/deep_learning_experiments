8fb8d8a3d78b1bc03f12f85c76ed0eafc67a77f8
Chris Grimm <cgrimm@gmail.com>
2016-07-04 00:37:22 -0400

    moop

--- models/variational_autoencoder/main.py
+++ models/variational_autoencoder/main.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
-from models.variational_autoencoder.VariationalAutoencoder import VAE_realize, VAE, log_normal_pdf
+from models.variational_autoencoder.VariationalAutoencoder import VAE_realize, VAE, log_normal_pdf, log_bernoulli_pmf
 sess = tf.Session()
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
@@ -11,12 +11,12 @@ x = tf.placeholder(tf.float32, [None, 28*28])
 encoding_params, enc_vars = VAE(x, 500, 20, 'gaussian', prefix='enc_')
 z_random = tf.placeholder(tf.float32, [None, 20])
 encoding = VAE_realize(encoding_params, z_random, 'gaussian')
-recon_params, dec_vars = VAE(encoding, 500, 28*28, 'gaussian', prefix='dec_')
+recon_params, dec_vars = VAE(encoding, 500, 28*28, 'bernoulli', prefix='dec_')
 #recon_random = tf.placeholder(tf.float32, [None, 28*28])
 #recon = VAE_realize(recon_params, recon_random, 'gaussian')
 # set up loss function and training
 q = log_normal_pdf(encoding, encoding_params[0], encoding_params[1])
-p = log_normal_pdf(x, recon_params[0], recon_params[1]) + \
+p = log_bernoulli_pmf(x, recon_params[0]) + \
     log_normal_pdf(encoding, tf.zeros_like(encoding_params[0]), tf.ones_like(encoding_params[1]))
 loss = tf.reduce_mean(-(p - q), reduction_indices=0)
 train = tf.train.AdamOptimizer(0.001).minimize(loss)
@@ -28,7 +28,7 @@ saver = tf.train.Saver(params)
 for name, item in params.items():
     print name, item
 sess.run(tf.initialize_all_variables())
-should_train = True
+should_train = False
 if should_train:
     batch_size = 100
     for i in range(20):
