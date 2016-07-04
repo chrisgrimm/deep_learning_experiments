d83042cfdbba55e4cb136d2f38ae0b14cd54a9d0
Chris Grimm <cgrimm@gmail.com>
2016-07-03 23:08:57 -0400

    moop

--- models/variational_autoencoder/main.py
+++ models/variational_autoencoder/main.py
@@ -12,11 +12,11 @@ encoding_params, enc_vars = VAE(x, 500, 20, 'gaussian', prefix='enc_')
 z_random = tf.placeholder(tf.float32, [None, 20])
 encoding = VAE_realize(encoding_params, z_random, 'gaussian')
 recon_params, dec_vars = VAE(encoding, 500, 28*28, 'gaussian', prefix='dec_')
-recon_random = tf.placeholder(tf.float32, [None, 28*28])
-recon = VAE_realize(recon_params, recon_random, 'gaussian')
+#recon_random = tf.placeholder(tf.float32, [None, 28*28])
+#recon = VAE_realize(recon_params, recon_random, 'gaussian')
 # set up loss function and training
 q = log_normal_pdf(encoding, encoding_params[0], encoding_params[1])
-p = log_normal_pdf(recon, recon_params[0], recon_params[1]) + \
+p = log_normal_pdf(x, recon_params[0], recon_params[1]) + \
     log_normal_pdf(encoding, tf.zeros_like(encoding_params[0]), tf.ones_like(encoding_params[1]))
 loss = tf.reduce_mean(-(p - q), reduction_indices=0)
 #loss = tf.reduce_mean(tf.reduce_mean(tf.pow(x - recon_params[0], 2), reduction_indices=1), reduction_indices=0)
@@ -37,7 +37,7 @@ if should_train:
         for y in range(len(mnist.train.images)/batch_size):
             data = mnist.train.next_batch(batch_size)
             feed_dict = {x: data[0], z_random: np.random.normal(size=(batch_size, 20)),
-                         recon_random: np.random.normal(size=(batch_size, 28*28))}
+                         }#recon_random: np.random.normal(size=(batch_size, 28*28))}
             _, ll = sess.run([train, loss], feed_dict=feed_dict)
             avg_loss.append(ll)
         print i
@@ -50,7 +50,7 @@ else:
     data = mnist.train.next_batch(batch_size)
     feed_dict = {x: data[0],
              z_random: np.random.normal(size=(batch_size, 20)),
-                 recon_random: np.random.normal(size=(batch_size, 28*28))}
+                 }#recon_random: np.random.normal(size=(batch_size, 28*28))}
     res_mu = sess.run(recon_params[0], feed_dict)
     f, [ax2, ax3] = plt.subplots(1, 2)
     #ax1.imshow(np.reshape(res, [28, 28]))
