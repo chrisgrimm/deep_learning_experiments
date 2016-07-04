1725240f534d3c7e07a0fc3702a7dde0f2143418
Chris Grimm <cgrimm@gmail.com>
2016-07-04 18:20:46 -0400

    got air running

--- models/attend_infer_repeat/main.py
+++ models/attend_infer_repeat/main.py
@@ -3,6 +3,9 @@ import numpy as np
 from models.attend_infer_repeat.AIR import AIR
 from data_generation import get_batch
 
+
+print 'about to start!'
+raw_input()
 sess = tf.Session()
 
 batch_size = 100
