c7e2d64162f05e8c5758523ab95b9412ce2fc803
vidurj <vidur94@gmail.com>
2016-07-06 00:53:08 -0400

    added weight sharing

--- models/attend_infer_repeat/main.py
+++ models/attend_infer_repeat/main.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from models.attend_infer_repeat.AIR import AIR
+from AIR import AIR
 from data_generation import get_batch
 
 sess = tf.Session()
@@ -19,3 +19,7 @@ for i in range(0, num_batches):
     batch = get_batch(batch_size)[0]
     loss = air.train_batch(batch, batch_size, i)
     print loss
+    if i % 1000 == 0:
+        print "Saving"
+        air.save()
+        print "saved"
