a0374aa4681ac0c45bf90f0f5edf6f0eae2fb8b9
Chris Grimm <cgrimm@gmail.com>
2016-07-19 17:13:08 -0400

    moop

--- models/scene_reconstruction/main.py
+++ models/scene_reconstruction/main.py
@@ -8,7 +8,7 @@ import numpy as np
 matplotlib.use('Agg')
 batch_size = 50
 frame_buffer_size = 1000
-w, h = 10, 10
+w, h = 50, 50
 # put initial frames in the buffer
 frame_buffer = RollingBuffer(frame_buffer_size)
 initial_frames = getFrames(frame_buffer_size)
@@ -20,22 +20,22 @@ for frame in initial_frames:
 # set up input for random frames.
 inp = tf.placeholder(tf.float32, shape=[None, 10, h, w])
 desired_outs = tf.placeholder(tf.float32, shape=[None, h, w])
-
 lstm = tf.nn.rnn_cell.BasicLSTMCell(h*w)
-state = tf.zeros(shape=[1, lstm.state_size])
+state = tf.zeros(shape=[batch_size, lstm.state_size])
 outs_list = []
 for i in range(10):
     with tf.variable_scope('lstm_scope') as scope:
         if i > 0:
             scope.reuse_variables()
-        output, state = lstm(tf.reshape(inp[:, i, :, :], [-1, h*w]), state, scope=scope)
+        tmp = tf.reshape(inp[:, i, :, :], [-1, h*w])
+        print tmp.get_shape()
+        output, state = lstm(tmp, state, scope=scope)
 output = tf.reshape(output, [-1, h, w])
 
 loss = tf.reduce_mean(tf.pow(output - desired_outs, 2))
 train_batch = tf.train.AdamOptimizer().minimize(loss)
 print 'here!'
 print w, h
-raw_input()
 
 sess = tf.Session()
 
@@ -53,7 +53,9 @@ def make_batch(frame_buffer, batch_size):
 
 while True:
     print i
-    feed_dict = {inp: make_batch(frame_buffer, batch_size), desired_outs: frame_buffer.sample(batch_size)}
+    inp_data = make_batch(frame_buffer, batch_size)
+    print inp_data.shape
+    feed_dict = {inp: inp_data, desired_outs: frame_buffer.sample(batch_size)}
     [_, bgs] = sess.run([train_batch, output], feed_dict)
     if i % 100 == 0:
         plt.imshow(bgs[0], cmap='Greys_r')
